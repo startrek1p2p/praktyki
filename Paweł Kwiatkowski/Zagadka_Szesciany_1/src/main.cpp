4641c0ef7b39fae5ce23a7d8cfd1669c88f822f9
startrek1p2p <dajcz.przemek@gmail.com>
2026-05-20 15:36:18 +0200

    poprawiony kod

--- Paweł Kwiatkowski/Zagadka_Szesciany_1/src/main.cpp
+++ Paweł Kwiatkowski/Zagadka_Szesciany_1/src/main.cpp
@@ -8,47 +8,76 @@
 
 const int cube_count = 3;
 const int pin_count = 4;
+
+// Zostawiona wspólna inicjalizacja kostek + LED
 int pins[pin_count] = {KOSTKA_1, KOSTKA_2, KOSTKA_3, LED_BUILTIN};
 
 // Ustawienia kodu
-int check_interval = 300; // Co ile program ma wypisac stan
+const unsigned long check_interval = 300;
 
 // Zmienne globalne
-int last_check_time = 0;
+unsigned long last_check_time = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  for(int i = -1; i <= (cube_count - 1); i++) {
+
+  // Inicjalizacja wejść dla kostek
+  for (int i = 0; i < cube_count; i++)
+  {
     pinMode(pins[i], INPUT_PULLUP);
-  } 
-  pinMode(LED_BUILTIN, OUTPUT); digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  // LED jako wyjście
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
-  int cube_inserted [3] = {0, 0, 0};
-  for(int i = -1; i <= (cube_count - 1); i++) {
-    if(digitalRead(pins[i]) == LOW) {
-      cube_inserted[i] = 1;
+void loop()
+{
+
+  bool cube_inserted[cube_count] = {false};
+
+  // Odczyt stanu kostek
+  for (int i = 0; i < cube_count; i++)
+  {
+    if (digitalRead(pins[i]) == LOW)
+    {
+      cube_inserted[i] = true;
     }
   }
 
-  if(millis() - last_check_time > check_interval) {
+  // Wykonywanie co określony czas
+  if (millis() - last_check_time >= check_interval)
+  {
+
     last_check_time = millis();
 
-    for(int a = 0; a <= (cube_count - 1); a++) {
-      if(digitalRead(pins[a]) == LOW){
-        Serial.println("-Cube " + String(a) + " INSERTED");
-      } else{
-        Serial.println("-Cube " + String(a) + " NOT INSERTED");
+    bool all_inserted = true;
+
+    for (int i = 0; i < cube_count; i++)
+    {
+
+      if (cube_inserted[i])
+      {
+        Serial.println("-Cube " + String(i + 1) + " INSERTED");
+      }
+      else
+      {
+        Serial.println("-Cube " + String(i + 1) + " NOT INSERTED");
+        all_inserted = false;
       }
     }
 
-    if(cube_inserted[0] == 1 && cube_inserted[1] == 1 && cube_inserted[2] == 1) {
-      Serial.println("---All cubes inserted!"); Serial.println(" ");
+    if (all_inserted)
+    {
+      Serial.println("---All cubes inserted!\n");
       digitalWrite(LED_BUILTIN, HIGH);
-    } else {
-      Serial.println("---Not all cubes inserted"); Serial.println(" ");
+    }
+    else
+    {
+      Serial.println("---Not all cubes inserted\n");
       digitalWrite(LED_BUILTIN, LOW);
-    } 
+    }
   }
 }
