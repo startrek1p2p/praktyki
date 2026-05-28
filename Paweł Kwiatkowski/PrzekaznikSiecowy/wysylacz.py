4a2ee451a8562ad15bc14f88ca0503654aeb744d
Paweł Kwiatkowski <110034652+Kwiatens@users.noreply.github.com>
2026-05-28 15:06:25 +0200

    Poprawka

--- Paweł Kwiatkowski/PrzekaznikSiecowy/wysylacz.py
+++ Paweł Kwiatkowski/PrzekaznikSiecowy/wysylacz.py
@@ -3,7 +3,7 @@ import sys
 
 # Przyciski dotykowe są na 192.168.1.220 port 100
 HOST, PORT = "192.168.1.220", 100
-data = " ".join(sys.argv[1:]).strip() or "OFF"
+data = " ".join(sys.argv[1:]).strip() or "ON"
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
